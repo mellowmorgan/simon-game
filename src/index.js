751a0e39ce08d72966c13b0eadbf32eef23efc73
mellowmorgan <morgan.waites@gmail.com>
2021-11-08 19:56:36 -0800

    Add click listeners to divs

--- src/index.js
+++ src/index.js
@@ -4,7 +4,48 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 
-let game = new Game();
-console.log(game.getPattern());
-console.log(game.getPattern());
+//function startRound;
 
+let clicked="none";
+function clickListeners(){
+  $("#start").on("click", function(){
+    let game = new Game();
+    //startRound();
+    let newPattern = game.getPattern();
+    for(let i=0;i<newPattern.length;i++){
+      $(this).delay(1500).queue(function() {
+        $("#game-blinker-box").css("background-color",newPattern[i]);
+        $(this).dequeue();
+     });
+    } 
+    $(this).delay(1000).queue(function() {
+      $("#game-blinker-box").css("background-color","grey");
+      $(this).dequeue();
+   });
+     for(let i=0;i<newPattern.length;i++){
+      $(this).delay(3000).queue(function() {
+        if(clicked===newPattern[i]){
+          alert("correct");
+          
+        }else{alert("wrong")}
+        $(this).dequeue(); 
+     });clicked="none";
+    }
+    
+  
+});
+$("#red").on("click", function(){
+  clicked=this.id;
+});
+$("#yellow").on("click", function(){
+  clicked=this.id;
+});
+$("#blue").on("click", function(){
+  clicked=this.id;
+});
+
+}
+
+
+
+clickListeners();
